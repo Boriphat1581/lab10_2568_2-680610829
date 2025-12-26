cc6fa3b608fefd7d4666a4fd9f9ca7c370aeb2e5
Boriphat1581 <boriphat_palee@cmu.ac.th>
2025-12-26 13:42:58 +0700

    lab10_3.cpp

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,34 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    int count = 0;
+    double sum = 0;
+    double sum_sq = 0;
+    double score;
+    
+    ifstream source("score.txt");
+    
+
+    while (source >> score) {
+        sum += score;       
+        sum_sq += score * score; 
+        count++;                
+    }
+
+
+    double mean = sum / count;
+    double sd = sqrt((sum_sq / count) - (mean * mean));
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3); 
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << sd << endl;
+
+    source.close();
+    return 0;
+}
